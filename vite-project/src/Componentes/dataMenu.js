0b611c60db0ee6858ec3a40ba761c1aeadf6320a
Douglas-Zuqueto <0001039234@senaimgaluno.com.br>
2024-05-27 18:41:18 -0300

    Adição de R$.

--- vite-project/src/Componentes/dataMenu.js
+++ vite-project/src/Componentes/dataMenu.js
@@ -23,49 +23,49 @@ export const menuPorcao = [
       id: 1,
       imageurl: "../src/assets/Img/aneis de cebola.jpg",
       name: "Aneis de Cebola",
-      price: "18,99"
+      price: "R$18,99"
   },
   {
       id: 2,
       imageurl: "../src/assets/Img/batata com file de peixe.jpg",
       name: "Batata com peixe",
-      price: "22,50"
+      price: "R$22,50"
   },
   {
       id: 3,
       imageurl: "../src/assets/Img/camarão.jpg",
       name: "Camarão",
-      price: "30,00"
+      price: "R$30,00"
   },
   {
       id: 4,
       imageurl: "../src/assets/Img/costelinha.jpg",
       name: "Costelinha",
-      price: "25,75"
+      price: "R$25,75"
   },
   {
       id: 5,
       imageurl: "../src/assets/Img/coxinhas de frango.jpg",
       name: "Coxa de frango",
-      price: "15,99"
+      price: "R$15,99"
   },
   {
       id: 6,
       imageurl: "../src/assets/Img/porção de bata bacon mussarela.jfif",
       name: "Batata com bacon",
-      price: "20,00"
+      price: "R$20,00"
   },
   {
       id: 7,
       imageurl: "../src/assets/Img/tiras de peixe com molho.jpg",
       name: "File de peixe",
-      price: "28,50"
+      price: "R$28,50"
   },
   {
       id: 8,
       imageurl: "../src/assets/Img/torresmo.jpg",
       name: "Torresminho",
-      price: "18,00"
+      price: "R$18,00"
   }
   ]
 export const menuHamburguer = [
@@ -73,49 +73,49 @@ export const menuHamburguer = [
       id: 1,
       imageurl: "../src/assets/Img/ham1.jfif",
       name: "Hambúrguer Clássico",
-      price: "18,99"
+      price: "R$18,99"
   },
   {
       id: 2,
       imageurl: "../src/assets/Img/hambuguer com 3 bife cheddar e bacon.jfif",
       name: "Hambúrguer com 3 Bifes, Cheddar e Bacon",
-      price: "25,99"
+      price: "R$25,99"
   },
   {
       id: 3,
       imageurl: "../src/assets/Img/hambuguer simples com cebola roxa.jfif",
       name: "Hambúrguer Simples com Cebola Roxa",
-      price: "15,99"
+      price: "R$15,99"
   },
   {
       id: 4,
       imageurl: "../src/assets/Img/hambuguer com dois bife cheddar e picles.jfif",
       name: "Hambúrguer com Dois Bifes, Cheddar e Picles",
-      price: "22,50"
+      price: "R$22,50"
   },
   {
       id: 5,
       imageurl: "../src/assets/Img/Hambúrguer Artesanal para vender.jfif",
       name: "Hambúrguer Artesanal",
-      price: "30,00"
+      price: "R$30,00"
   },
   {
       id: 6,
       imageurl: "../src/assets/Img/hamburguer com dois bifes bacon cheddar.jfif",
       name: "Hambúrguer com Dois Bifes, Bacon e Cheddar",
-      price: "28,50"
+      price: "R$28,50"
   },
   {
       id: 7,
       imageurl: "../src/assets/Img/Ideias de comida para vender no delivery_ Hambúrguer gourmet.jfif",
       name: "Hambúrguer Gourmet",
-      price: "35,00"
+      price: "R$35,00"
   },
   {
       id: 8,
       imageurl: "../src/assets/Img/rosquinha de hambuguer bacon ovo cheddar.jfif",
       name: "Rosquinha de Hambúrguer com Bacon, Ovo e Cheddar",
-      price: "20,00"
+      price: "R$20,00"
   }
   ]
 export const menuHotD = [
@@ -123,48 +123,48 @@ export const menuHotD = [
       id: 1,
       imageurl: "../src/assets/Img/cachorro quente com bastante molho.jfif",
       name: "Cachorro Quente com Bastante Molho",
-      price: "12,99"
+      price: "R$12,99"
   },
   {
       id: 2,
       imageurl: "../src/assets/Img/cachorro quente com molho vermelho.jfif",
       name: "Cachorro Quente com Molho Vermelho",
-      price: "13,99"
+      price: "R$13,99"
   },
   {
       id: 3,
       imageurl: "../src/assets/Img/cachorro quente com palmito.jfif",
       name: "Cachorro Quente com Palmito",
-      price: "14,99"
+      price: "R$14,99"
   },
   {
       id: 4,
       imageurl: "../src/assets/Img/cachorro quente simples.jfif",
       name: "Cachorro Quente Simples",
-      price: "10,99"
+      price: "R$10,99"
   },
   {
       id: 5,
       imageurl: "../src/assets/Img/calabresa.jpg",
       name: "Calabresa",
-      price: "11,99"
+      price: "R$11,99"
   },
   {
       id: 6,
       imageurl: "../src/assets/Img/cachorro quente com pedaços de carne.jfif",
       name: "Cachorro Quente com Pedaços de Carne",
-      price: "15,99"
+      price: "R$15,99"
   },
   {
       id: 7,
       imageurl: "../src/assets/Img/dogão.jpg",
       name: "Dogão",
-      price: "16,99"
+      price: "R$16,99"
   },
   {
       id: 8,
       imageurl: "../src/assets/Img/The Most Insane Hot Dogs From America's Baseball Stadiums.jfif",
       name: "Cachorro Quente Insano",
-      price: "18,99"
+      price: "R$18,99"
   }
   ]
